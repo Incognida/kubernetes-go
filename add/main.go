5323b7ce636e3086b3c76cde8179aa95f2e7192d
heroniques <zaur@izibits.io>
2019-07-14 16:20:19 +0300

    123

---
+++ add/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Incognida/kubernetes-go/add/pb"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+type server struct {}
+
+func (s *server) Compute(cxt context.Context, r *pb.AddRequest) (*pb.AddResponse, error) {
+	result := &pb.AddResponse{}
+	result.Result = r.A + r.B
+
+	logMessage := fmt.Sprintf("A: %d   B: %d     sum: %d", r.A, r.B, result.Result)
+	log.Println(logMessage)
+
+	return result, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Failed to listen:  %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb2.RegisterAddServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
---
+++ api/go.mod
@@ -0,0 +1,10 @@
+module github.com/Incognida/kubernetes-go/api
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/gorilla/mux v1.7.3
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7
+	google.golang.org/grpc v1.22.0
+)
